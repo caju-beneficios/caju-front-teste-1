4fc64c64e6d749d81ea78901f6243ccbf26d5a28
Luan Tonin Galvan <ltoningalvan@gmail.com>
2024-07-18 18:01:24 -0300

    style: adiciona configuração de lint para suportar os paths

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,37 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:import/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'eslint-config-prettier',
-    "plugin:react-hooks/recommended"
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
   ],
   settings: {
     react: {
-      version: 'detect',
+      version: "detect",
     },
-    'import/resolver': {
-      node: {
-        paths: ['src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
+    "import/resolver": {
+      typescript: {},
     },
   },
+  plugins: ["@typescript-eslint", "import"],
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "react/react-in-jsx-scope": "off",
     "import/no-named-as-default": 0,
-    "import/no-unresolved": ["error", { "ignore": ["^@/"] }],
+    "import/no-unresolved": ["error", { ignore: ["^@/"] }],
     "react/display-name": "off",
     "@typescript-eslint/ban-types": "off",
     "import/named": "off",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "@typescript-eslint/unbound-method": "off",
+  },
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: "./",
   },
 };
