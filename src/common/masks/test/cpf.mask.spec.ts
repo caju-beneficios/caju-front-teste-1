3ef64f3488d3e6d708c44ae77160963dbb4b8d37
Luan Tonin Galvan <ltoningalvan@gmail.com>
2024-07-20 13:57:32 -0300

    test: testa máscaras e validadores

---
+++ src/common/masks/test/cpf.mask.spec.ts
@@ -0,0 +1,16 @@
+import { maskCpf, unmaskCpf } from "../cpf.mask";
+
+describe("CPF Mask", () => {
+  it("should be able to mask a CPF", () => {
+    expect(maskCpf("")).toBe("");
+    expect(maskCpf("1111")).toBe("111.1");
+    expect(maskCpf("11111111111")).toBe("111.111.111-11");
+    expect(maskCpf("111111111111")).toBe("111.111.111-11");
+  });
+
+  it("should be able to unmask a CPF", () => {
+    expect(unmaskCpf("")).toBe("");
+    expect(unmaskCpf("111.1")).toBe("1111");
+    expect(unmaskCpf("000.111.111-11")).toBe("00011111111");
+  });
+});
---
+++ src/common/validations/test/cpf.validation.spec.ts
@@ -0,0 +1,8 @@
+import { validateCpf } from "../cpf.validation";
+
+test("should be able to validate a CPF", () => {
+  expect(validateCpf("")).toBe(false);
+  expect(validateCpf("86869331499")).toBe(true);
+  expect(validateCpf("131.637.571-46")).toBe(true);
+  expect(validateCpf("111.111.111-11")).toBe(false);
+});
