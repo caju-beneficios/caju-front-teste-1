76f8d4d8212d6afe2891a2919ee24c2c1ffa8e6b
Luan Tonin Galvan <ltoningalvan@gmail.com>
2024-07-20 12:11:59 -0300

    test: configura cypress

---
+++ cypress/e2e/create-a-new-user.cy.ts
@@ -0,0 +1,12 @@
+describe("Create a new user", () => {
+  it("should be able to create a new user", () => {
+    cy.visit("http://localhost:3001/#/new-user");
+
+    cy.get("input[name=name]").type("John Doe");
+    cy.get("input[name=email]").type("john.doe@email.com");
+    cy.get("input[name=document_number]").type("54315012327");
+    cy.get("input[name=date]").type("2024-07-19");
+
+    cy.get("button[data-testid=submit-form-button]").click();
+  });
+});
