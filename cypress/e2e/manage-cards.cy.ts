55266217bf25ea092ba233cdadf60b27a1d11d5d
Luan Tonin Galvan <ltoningalvan@gmail.com>
2024-07-21 19:13:34 -0300

    feat: testa busca

--- cypress/e2e/manage-cards.cy.ts
+++ cypress/e2e/manage-cards.cy.ts
@@ -20,4 +20,18 @@ describe("Create a new user", () => {
     cy.get("div[data-testid=registration-card-ee3e] .delete-button").click();
     cy.get("button[data-testid=confirm-button]").click();
   });
+
+  it("sould be able to search for a user", () => {
+    cy.visit("http://localhost:3001/#/dashboard");
+
+    cy.get("div[data-testid=registration-card-3274]").should("exist");
+
+    cy.get("input[type=search]").type("914.000.780-40");
+
+    cy.get("div[data-testid=registration-card-3274]").should("not.exist");
+
+    cy.get("input[type=search]").clear();
+
+    cy.get("div[data-testid=registration-card-3274]").should("exist");
+  });
 });
