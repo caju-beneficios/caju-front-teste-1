a3a4474c2b4e15719515ec2c9ce5aa0b9e5ca8bb
Felipe Oliveira <felpd3v@gmail.com>
2024-08-04 14:17:01 -0300

    feat(service): creates a `Registration` service for dealing with http requests

---
+++ src/services/registration/index.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import { queryClient } from "~/App";
+import { Registration, RegistrationStatus } from "~/types/registration";
+
+const endpoint = "http://localhost:3000/registrations";
+
+const fetchAll = async (
+  registration: Partial<Registration>
+): Promise<Registration[]> => {
+  const queryParams = new URLSearchParams(registration);
+  const response = await axios.get(`${endpoint}?${queryParams}`);
+  return response.data as Registration[];
+};
+
+const updateStatus = async (
+  registration: Registration,
+  status: RegistrationStatus
+): Promise<void> => {
+  if (registration.status !== status) {
+    await axios.put(`${endpoint}/${registration.id}`, {
+      ...registration,
+      status,
+    });
+
+    await queryClient.invalidateQueries("registrations");
+  }
+};
+
+const remove = async (id: string): Promise<void> => {
+  await axios.delete(`${endpoint}/${id}`);
+  await queryClient.invalidateQueries("registrations");
+};
+
+const create = async (registration: Partial<Registration>) => {
+  await axios.post(`${endpoint}`, registration);
+  await queryClient.invalidateQueries("registrations");
+};
+
+export const RegistrationService = {
+  fetchAll,
+  updateStatus,
+  remove,
+  create,
+};
